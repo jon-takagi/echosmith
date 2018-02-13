7556396d190b77c6108f9b924dbeecf2cd50906e
Jon Takagi <jon.takagi@icloud.com>
2018-02-13 07:36:47 +0000

    this works on remote servers now - turns out just copying code from tutorialpoint and not testing doesn't work

--- echo-server.py
+++ echo-server.py
@@ -2,11 +2,16 @@ import socket
 s = socket.socket()
 host = socket.gethostname()
 port = 42069
-s.bind((host, port))
+s.bind(('0.0.0.0', port))
 
 s.listen(5)
 while True:
 	client, addr = s.accept()
-	print("connection from: ", addr)
-	client.send(b'ping')
-	client.close()
+	while True:
+		data = client.recv(2048)
+		if data == b'quit\r\n':
+			client.shutdown(1)
+			client.close()
+		else:
+			client.send(data)
+			print(data)
